6f8c6b79d7890c438a64382c41749399b964583b
asdru0510 <sifontesupta@gmail.com>
2024-08-24 11:31:42 -0500

    fix a modelos tipo de datos a description y agregar related_name a foreignkey de Subtarea

--- desafioadl/models.py
+++ desafioadl/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Tarea(models.Model):
-    description=models.CharField(default="")
+    description=models.TextField(default="")
     eliminada=models.BooleanField(default=False)
 
 class Subtarea(models.Model):
-    tarea_id = models.ForeignKey(Tarea, on_delete=models.CASCADE)
-    description=models.CharField(default="")
+    tarea_id = models.ForeignKey(Tarea, on_delete=models.CASCADE, related_name='subtarea')
+    description=models.TextField(default="")
     eliminada=models.BooleanField(default=False)
